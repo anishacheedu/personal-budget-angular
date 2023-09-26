f929f6d034afd055d3a63eccc48353e7545a0269
Anisha Cheedu <cheeduanishareddy@gmail.com>
2023-09-25 23:48:43 -0400

    Added Angular

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,131 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component  } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Chart } from 'chart.js/auto';
-import * as d3 from 'd3';
-import { DataService } from '../data.service';
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'pb-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss'],
+  styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
-  width = 600;
-  height = 300;
-  radius = Math.min(this.width, this.height) / 2;
-  svg:any;
-  color:any;
-  pie:any;
-  keya:any;
-  arc:any;
-  outerArc:any;
-  dataReady: any;
-  dataSource:any;
+export class HomepageComponent implements AfterViewInit {
+  public dataSource={
+    datasets:[
+        {
+            data:[30,350,90],
+            backgroundColor: [
+                    '#ffcd56',
+                    '#ff6384',
+                    '#36a2eb',
+                    '#fd6b19'
+                ],
+        }
+    ],
+    labels:[
+        'Eat out',
+        'Rent',
+        'Groceries',
+    ]
+};
+private data: any[] = [
+  { Stars: 5, Framework: 'Eat out' },
+  { Stars: 4, Framework: 'Rent' },
+  { Stars: 3, Framework: 'Groceries' }
+];
 
-  constructor(private http: HttpClient, private dataService: DataService) {
-  }
+  private svg: any;
+  private margin = 50;
+  private width = 750;
+  private height = 600;
+  private radius = Math.min(this.width, this.height) / 2 - this.margin;
+  private colors : any;
 
-  ngOnInit(): void {
-    // async data update from data service using setTimeOut
-        setTimeout(() => {
-            this.dataSource = this.dataService.dataSource;
-            this.createChart();
-            this.draw();
-          }, 1000);
-  }
 
-  private createChart(): void {
-    const ctx:any = document.getElementById('myChart');
-    const myPieChart = new Chart(ctx, {
-      type: 'pie',
-      data: this.dataSource,
-    });
-  }
 
-  private getData() {
-    const arr = [];
-    const labels = this.dataSource.labels;
-    for (let i = 0; i < this.dataSource.datasets[0].data.length; i++) {
-      arr.push({
-        label: labels[i],
-        value: this.dataSource.datasets[0].data[i],
+
+
+
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit(): void {
+    this.http.get('http://localhost:3000/budget').subscribe((res:any) => {
+    for(var i=0;i<res.myBudget.length;i++){
+        this.dataSource.datasets[0].data[i]=res.myBudget[i].budget;
+        this.dataSource.labels[i]=res.myBudget[i].title;
+    }
+    this.createChart();
+
+
+  });
+}
+  createChart(){
+      var ctx = document.getElementById('myChart') as HTMLCanvasElement;
+      var myPieChart = new Chart(ctx,{
+          type:'pie',
+          data:this.dataSource,
       });
     }
-    return arr;
-  }
 
-  private midAngle(d: { startAngle: number; endAngle: number; }) {
-    return d.startAngle + (d.endAngle - d.startAngle) / 2;
-  }
-
-  private draw(): void {
-    this.svg = d3.select('svg')
-               .attr('width', this.width)
-               .attr('height', this.height)
-               .append('g')
-               .attr('transform', 'translate(' + this.width / 2 + ',' +
-                this.height / 2 + ')');
-    this.radius = Math.min(this.width, this.height) / 2 ;
-
-    this.color = d3.scaleOrdinal()
-            .domain(this.dataSource.labels)
-           .range(["#ffcd56", "#ff6384", "#36a2eb", "#fd6b19", "#6e7abb", "#95e982", "#b839d8"]);
-
-    this.pie = d3.pie()
-    .sort(null)
-               .value((d: any) => d.value);
-
-    this.dataReady = this.pie(this.getData());
-
-    this.arc = d3.arc()
-    .innerRadius(this.radius * 0.5)
-    .outerRadius(this.radius * 0.8);
-
-    this.outerArc = d3.arc()
-    .innerRadius(this.radius * 0.9)
-    .outerRadius(this.radius * 0.9);
-
-    this.svg
-  .selectAll('allSlices')
-  .data(this.dataReady)
-  .enter()
-  .append('path')
-  .attr('d', this.arc)
-  .attr('fill', (d:any) => (this.color(d.data.label)))
-  .attr('stroke', 'white')
-  .style('stroke-width', '2px')
-  .style('opacity', 0.7);
-
-    this.svg
-  .selectAll('allPolylines')
-  .data(this.dataReady)
-  .enter()
-  .append('polyline')
-    .attr('stroke', 'black')
-    .style('fill', 'none')
-    .attr('stroke-width', 1)
-    .attr('points', (d:any) => {
-        const posA = this.arc.centroid(d);
-        const posB = this.outerArc.centroid(d);
-        const posC = this.outerArc.centroid(d);
-        posC[0] = this.radius * 0.95 * (this.midAngle(d) < Math.PI ? 1 : -1);
-        return [posA, posB, posC];
-      });
-
-
-    this.svg
-      .selectAll('allLabels')
-      .data(this.dataReady)
-      .enter()
-      .append('text')
-        .text( (d:any) => { console.log(d.data.label); return d.data.label; } )
-        .attr('transform', (d:any) => {
-            const pos = this.outerArc.centroid(d);
-            pos[0] = this.radius * 0.99 * (this.midAngle(d) < Math.PI ? 1 : -1);
-            return 'translate(' + pos + ')';
-          })
-        .style('text-anchor', (d:any) => {
-            return (this.midAngle(d) < Math.PI ? 'start' : 'end');
-          });
-    }
 }
